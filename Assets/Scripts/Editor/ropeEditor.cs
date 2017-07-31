1ab0eb9822cd4863d35d2010775f1e85986b8a74
Jake Perry <jake-perry@live.com>
2017-07-31 13:50:20 +0930

    Rope regeneration, refactored code
    Created custom editor for RopeController, which allows the rope to be regenerated with new parameters.
    Refactored RopeController class to allow regeneration. Regenerate & Create functions now independently set up the beginning of the rope, then call the AddSegments function to procedurally generate the rope. RopeController now also stores a reference to the first segment of the rope. As this is the piece which the RopeController script is assigned to when created from the editor, this is not deleted when regenerating the rope.
    RopeController no longer uses lineRenderer variables introduced in 5.6.1f1, in order to keep compatability. 
    RopeController now attempts to auto-assign the rope material via a Resources lookup if no material has been assigned.

---
+++ Assets/Scripts/Editor/ropeEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(RopeController))]
+public class ropeEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        DrawDefaultInspector();
+
+        RopeController instance = target as RopeController;
+        if (GUILayout.Button(new GUIContent("Regenerate")))
+        {
+            instance.RegenerateRope();
+        }
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
--- Assets/Scripts/RopeController.cs
+++ Assets/Scripts/RopeController.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class RopeController : MonoBehaviour
 {
-    // TODO: Create rope script to hold references to each rope segment. update positions in lateupdate
+    [Range(1, 100), SerializeField] private uint _segments = 5;
+    [Range(1.0f, 100.0f), SerializeField]  private float _length = 5.0f;
+    [SerializeField, HideInInspector]   private Rigidbody _rigidbody = null;
+
     [Range(0.1f, 0.4f), SerializeField]    private float _width = 0.15f;
     [SerializeField]    private Material _ropeMaterial = null;
-    [SerializeField]    private Vector2 _materialScale = new Vector2(5, 1);
     private LineRenderer _line = null;
 
+    [SerializeField, HideInInspector]   private GameObject _firstSegment = null;
     [SerializeField, HideInInspector]   private List<Transform> _segmentTransforms = new List<Transform>();
     [SerializeField, HideInInspector]   private List<Vector3> _positionCache = new List<Vector3>();
 
@@ -20,33 +23,89 @@ public class RopeController : MonoBehaviour
         _line.startWidth = _width;
         _line.endWidth = _width;
 
-        _line.positionCount = 0;
-        _line.textureMode = LineTextureMode.RepeatPerSegment;
+        _line.numCapVertices = 5;
+        _line.numCornerVertices = 6;
+
+        _line.SetPositions(new Vector3[0]);
+        _line.textureMode = LineTextureMode.Tile;
+
+        // Retrieve rope material
+        if (_ropeMaterial == null)
+            _ropeMaterial = Resources.Load<Material>("Materials/RopeMaterial");
 
         if (_ropeMaterial != null)
-        {
             _line.material = _ropeMaterial;
-            _ropeMaterial.SetTextureScale("_MainTex", _materialScale);
-        }
     }
 	
 	void Update ()
     {
         // Get all segment positions
         _positionCache.Clear();
+        _positionCache.Add(_firstSegment.transform.position);
         foreach (Transform t in _segmentTransforms)
         {
             _positionCache.Add(t.position);
         }
 
         // Set line renderer to draw over these segments
-        _line.positionCount = _positionCache.Count;
+        _line.numPositions = _positionCache.Count;
         _line.SetPositions(_positionCache.ToArray());
 	}
 
     private void AddSegmentTransform(Transform t)
     {
         _segmentTransforms.Add(t);
+    }
+
+    private void AddSegments(uint segments, float ropeLength)
+    {
+        if (!_firstSegment || !_rigidbody)
+            return;
+
+        float segmentSpacing = ropeLength / (float)segments;
+        float segmentSpacingHalf = segmentSpacing / 2;
+        float colliderRadius = Mathf.Min(segmentSpacing / 10, _width / 2);
+
+        // Create segments of rope
+        Transform previousTransform = _firstSegment.transform;
+        Rigidbody previousRB = _rigidbody;
+        for (uint i = 0; i < segments; i++)
+        {
+            // Create a new gameobject for the segment
+            Vector3 pos = new Vector3(0, -((i + 1) * segmentSpacing), 0) + previousTransform.position;
+
+            GameObject segmentEnd = new GameObject();
+            segmentEnd.transform.parent = _firstSegment.transform;
+            segmentEnd.name = "Segment " + i.ToString();
+            segmentEnd.transform.position = pos;
+
+            // Track this segment's transform
+            AddSegmentTransform(segmentEnd.transform);
+
+            // Add a rigidbody, and a hinge joint connected to the previous segment
+            Rigidbody segmentRB = segmentEnd.AddComponent<Rigidbody>();
+            HingeJoint segmentHinge = segmentEnd.AddComponent<HingeJoint>();
+            segmentHinge.connectedBody = previousRB;
+
+            // Add collider
+            SphereCollider collider = segmentEnd.AddComponent<SphereCollider>();
+            collider.radius = colliderRadius;
+
+            // Track this as previous rigidbody for use with next segment
+            previousRB = segmentRB;
+        }
+    }
+
+    public void RegenerateRope()
+    {
+        // Destroy all old segments
+        foreach (Transform t in _segmentTransforms)
+            DestroyImmediate(t.gameObject);
+
+        _segmentTransforms.Clear();
+
+        // Generate new segments
+        AddSegments(_segments, _length);
     }
 
     public static void CreateNewRope(Vector3 localPosition, GameObject parent = null, uint segments = 5, float length = 5)
@@ -63,40 +122,16 @@ public class RopeController : MonoBehaviour
 
         // Add a RopeController script
         RopeController ropeController = ropeEmpty.AddComponent<RopeController>();
-        ropeController.AddSegmentTransform(ropeEmpty.transform);
+        ropeController._firstSegment = ropeEmpty;
 
-        Rigidbody ropeRB = ropeEmpty.AddComponent<Rigidbody>();
-        ropeRB.isKinematic = true;
+        // Add a rigidbody
+        ropeController._rigidbody = ropeEmpty.AddComponent<Rigidbody>();
+        ropeController._rigidbody.isKinematic = true;
 
-        float segmentSpacing = length / (float)segments;
-
-        // Create segments of rope
-        Transform previousTransform = ropeEmpty.transform;
-        Rigidbody previousRB = ropeRB;
-        for (uint i = 0; i < segments; i++)
-        {
-            // Create a new gameobject for the segment
-            Vector3 pos = new Vector3(0, -((i + 1) * segmentSpacing), 0) + previousTransform.position;
-
-            GameObject segmentEnd = new GameObject();
-            segmentEnd.transform.parent = ropeEmpty.transform;
-            segmentEnd.name = "Segment " + i;
-            segmentEnd.transform.position = pos;
-
-            // Track this segment's transform
-            ropeController.AddSegmentTransform(segmentEnd.transform);
-
-            // Add a rigidbody, and a hinge joint connected to the previous segment
-            Rigidbody segmentRB = segmentEnd.AddComponent<Rigidbody>();
-            HingeJoint segmentHinge = segmentEnd.AddComponent<HingeJoint>();
-            segmentHinge.connectedBody = previousRB;
-
-            // Add collider
-            SphereCollider collider = segmentEnd.AddComponent<SphereCollider>();
-            collider.radius = segmentSpacing / 3;
-
-            // Track this as previous rigidbody for use with next segment
-            previousRB = segmentRB;
-        }
+        // Generate new segments
+        ropeController.AddSegments(segments, length);
+        
+        // Load rope material
+        ropeController._ropeMaterial = Resources.Load<Material>("Materials/RopeMaterial");
     }
 }
