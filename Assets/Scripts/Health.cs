a1bc32f31bd3e096d12e99f80ca55dc1e02ee086
Jake Perry <jake-perry@live.com>
2017-10-20 11:13:59 +1030

    Particle collisions
    Added Unity's example particle effects asset.
    Added basic health script which takes damage from particle collision.

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField]
+    private float value = 100.0f;
+
+    public float health { get { return value; } }
+
+    public delegate void DamageDelegate(Health sender);
+    public event DamageDelegate OnDamage;
+
+    private void OnParticleCollision(GameObject g)
+    {
+        value -= 15.0f * Time.deltaTime;
+
+        if (value <= 0.0f)
+            value = 100.0f;
+
+        if (OnDamage != null)
+            OnDamage.Invoke(this);
+    }
+}
---
+++ Assets/Scripts/HealthText.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthText : MonoBehaviour
+{
+    private TMPro.TMP_Text text = null;
+
+    private void Awake()
+    {
+        text = GetComponent<TMPro.TMP_Text>();
+
+        Health health = FindObjectOfType<Health>();
+        if (health)
+            health.OnDamage += OnDamage;
+    }
+
+    private void OnDamage(Health sender)
+    {
+        if (text)
+        {
+            text.text = string.Format("Health: {0}", sender.health.ToString("F2"));
+            text.rectTransform.anchoredPosition = Vector3.zero;
+        }
+    }
+}
